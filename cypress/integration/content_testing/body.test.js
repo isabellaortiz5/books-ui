4c1bd577e70158f1638bd6413f5e61535c925b33
SeanQO <seanquintero3@gmail.com>
2022-03-26 17:22:31 -0500

    added body func tests

---
+++ cypress/integration/content_testing/body.test.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress"/>
+
+describe("delete user func test", () =>{
+    beforeEach(() =>{
+        cy.visit('http://localhost:4200/dashboard')
+    }) 
+
+    it("Loaded body state",() =>{
+        cy.get('[nztype="default"]')
+        .invoke('attr',"ng-reflect-disabled").should("eq","true")
+
+        cy.get(".table-button.ant-btn.ant-btn-primary").should("be.visible")
+
+        cy.get(':nth-child(n) > :nth-child(n) > .ant-btn').should("be.visible") 
+    })
+
+    it("check all boxes",() =>{
+        cy.get('.ant-table-selection > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').check()
+        
+        cy.get(':nth-child(n) > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input')
+        .invoke('attr',"ng-reflect-model")
+        .should("eq","true")
+
+        cy.get('[nztype="default"]')
+        .invoke('attr',"ng-reflect-disabled").should("eq","false")
+    })
+
+    it("check all boxes",() =>{
+        cy.get(':nth-child(1) > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input')
+        .check()
+
+        cy.get('span.ant-checkbox-indeterminate').should("exist")
+
+        cy.get('[nztype="default"]')
+        .invoke('attr',"ng-reflect-disabled").should("eq","false")
+    })
+
+})
--- cypress/integration/content_testing/delete.test.js
+++ cypress/integration/content_testing/delete.test.js
@@ -26,13 +26,6 @@ describe("delete user func test", () =>{
         cy.get('[nztype="default"]')
         .invoke('attr',"ng-reflect-disabled")
         .should("eq","false")
-
-        cy.get('span.ant-checkbox-indeterminate').should("exist")
     })
 
 })
-
-
-
-
-
